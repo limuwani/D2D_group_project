7222692c1281b10a56243d234b7444f50237e24c
Sphephelo Hlongwa <205558120+sphe-hlongwa@users.noreply.github.com>
2026-04-25 19:45:54 +0300

    Fixing The Fixed Fix

--- app/src/main/java/com/example/d2d/MainActivity.java
+++ app/src/main/java/com/example/d2d/MainActivity.java
@@ -1,66 +1,37 @@
 package com.example.d2d;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
-
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.d2d.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        showLoginScreen();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private void showLoginScreen() {
+        setContentView(R.layout.login_page);
+
+        Button signUpBtn = findViewById(R.id.sign_up_button);
+        Button signInBtn = findViewById(R.id.sign_in_button);
+
+        signUpBtn.setOnClickListener(v -> showSignUpScreen());
+        signInBtn.setOnClickListener(v -> showCustomerScreen());
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void showSignUpScreen() {
+        setContentView(R.layout.sign_up);
+
+        Button backToLoginBtn = findViewById(R.id.back_to_login_button);
+        backToLoginBtn.setOnClickListener(v -> showLoginScreen());
+    }
+
+    private void showCustomerScreen() {
+        setContentView(R.layout.customer_page);
+        // Add more logic here if needed (e.g. back button to login)
     }
 }
+
