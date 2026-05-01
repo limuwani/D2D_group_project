6d2ba1ea82193310ebe10deac5cca1187fe9df63
nthabi2906 <naledi2mofokeng@gmail.com>
2026-05-01 17:21:24 +0200

    Classes Added And Main Activity Modified

--- app/src/main/java/com/example/d2d/MainActivity.java
+++ app/src/main/java/com/example/d2d/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.d2d;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         showLoginScreen();
+        setContentView(R.layout.login_page);
+        EditText email = findViewById(R.id.email_edit_text);
+        password = findViewById(R.id.password_edit_text);
+        Login = findViewById(R.id.sign_in_button);
     }
 
+    @SuppressLint("UseCompatTextViewDrawableApis")
+    public void onLoginClick(View v){
+        String Email = email.getText().toString();
+        String Pass = password.getText().toString().trim();
+        if(Email.isEmpty()){
+            email.setCompoundDrawableTintList(ColorStateList.valueOf(Color.RED));
+        }
+        if(Pass.isEmpty()){
+            password.setCompoundDrawableTintList(ColorStateList.valueOf(Color.RED));
+        }
+    }
     private void showLoginScreen() {
         setContentView(R.layout.login_page);
 
@@ -34,4 +58,4 @@ public class MainActivity extends AppCompatActivity {
         // Add more logic here if needed (e.g. back button to login)
     }
 }
-
+
